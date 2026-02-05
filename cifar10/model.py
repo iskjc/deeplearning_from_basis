a19e58434d9ef139ec973a2f6e219e3084b5766b
iskjc <srj15800897519@qq.com>
2026-02-05 23:28:31 +0800

    add resnet18(ez)

--- cifar10/model.py
+++ cifar10/model.py
@@ -6,9 +6,11 @@ class SimpleCNN(nn.Module):
         super(SimpleCNN, self).__init__()
         self.features = nn.Sequential(
             nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2), # 32 -> 16
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1), 
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2)  # 16 -> 8
         )
@@ -16,6 +18,7 @@ class SimpleCNN(nn.Module):
             nn.Flatten(),
             nn.Linear(64 * 8 * 8, 256),
             nn.ReLU(),
+            nn.Dropout(0.5),
             nn.Linear(256, 10)
         )
 
